187cf624e82ac6a691c7adfcf7e027e8e7ef9e4e
duduike <eduardo.happ@yahoo.com>
2023-07-01 17:27:03 -0300

    refactor: component name

--- fashion4u-e-commerce/src/app/components/productsWrapper/productsWrapper.tsx
+++ fashion4u-e-commerce/src/app/components/productsWrapper/productsWrapper.tsx
@@ -3,7 +3,7 @@ type ProductsWrapperProps = {
     children: React.ReactNode;
   };
   
-  export const ProductsArea: React.FC<ProductsWrapperProps> = ({ children }) => {
+  export const ProductsWrapper: React.FC<ProductsWrapperProps> = ({ children }) => {
     return (
       <div className="p-6 md:p-10">
         {children}
