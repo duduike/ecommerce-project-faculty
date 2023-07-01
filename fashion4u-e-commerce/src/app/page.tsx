839381907c6a2874f1ddb24f99af0b4fba9bd83d
duduike <eduardo.happ@yahoo.com>
2023-07-01 17:05:50 -0300

    feat: create a components of products section

--- fashion4u-e-commerce/src/app/page.tsx
+++ fashion4u-e-commerce/src/app/page.tsx
@@ -1,9 +1,17 @@
+
 import { Banner } from "./components/banner/banner";
+import { Products } from "./components/products/page";
+import { ProductsArea } from "./components/productsWrapper/productsWrapper";
+import { TitleSection } from "./components/titleSection/titleSection";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <ProductsArea>
+        <TitleSection title="Promoções" />
+        <Products />
+      </ProductsArea>
     </main>
   )
 }
